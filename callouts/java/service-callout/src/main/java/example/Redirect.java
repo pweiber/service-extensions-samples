102564ac50f2d4f9278c9ed29a94fa50a9b0a1e0
victorwoli <victorwo@ciandt.com>
2024-10-24 09:45:24 -0300

    Test3

--- callouts/java/service-callout/src/main/java/example/Redirect.java
+++ callouts/java/service-callout/src/main/java/example/Redirect.java
@@ -110,9 +110,6 @@ public class Redirect extends ServiceCallout {
         // Create a builder for ServiceCallout with custom configuration
         Redirect server = new Redirect.Builder()
                 .build();
-
-
-
         // Start the server and block until shutdown
         server.start();
         server.blockUntilShutdown();
